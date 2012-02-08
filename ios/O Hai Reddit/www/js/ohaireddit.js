d61b1d333731eb7a7646ab660b8e353009e09691
Tim Kim <timk@adobe.com>
2012-02-08 14:35:57 -0800

    Restyled how main list looks

--- ios/O Hai Reddit/www/js/ohaireddit.js
+++ ios/O Hai Reddit/www/js/ohaireddit.js
@@ -17,7 +17,8 @@ var getMoar= function(qs){
         var j = children.length;
         nextPage = data.data.after;
         for(var i=0;i<j;++i){
-            $('#theTable').append('<tr><td>'+children[i].data.score+'</td><td>'+children[i].data.title+'</td></tr>');
+            // should really template this godawful string out
+            $('#theTable').append('<tr><td><a id="'+children[i].data.id+'" href="#">'+children[i].data.title+'</a></td><td rowspawn="2"><a href="" class="btn"><i class="icon-comment"></i>'+children[i].data.num_comments+'</a></td></tr><tr><td><span class="label label-warning">'+children[i].data.ups+'</span><span class="label label-info">'+children[i].data.subreddit+'</span><span class="label label-success">'+children[i].data.author+'</span></td></tr>');
         }
         $('#moarButton').button('reset');
       }
