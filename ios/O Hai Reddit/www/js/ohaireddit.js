3d0dc2ecae51640f89db1e4554e2e3f4f338356d
Tim Kim <timk@adobe.com>
2012-02-07 17:44:37 -0800

    Got moar button working

--- ios/O Hai Reddit/www/js/ohaireddit.js
+++ ios/O Hai Reddit/www/js/ohaireddit.js
@@ -1,14 +1,25 @@
 var self = this;
+var nextPage = '';
 
-$.ajax({
-  url: 'http://reddit.com/.json',
-  dataType: 'json',
-  success: function(data){
-    var children = data.data.children;
-    var j = children.length;
-    for(var i=0;i<j;++i){
-        $('#theTable').append('<tr><td>'+children[i].data.score+'</td><td>'+children[i].data.title+'</td></tr>');
-    }
-  }
+$('#moarButton').on('click',function(){
+    $('#moarButton').button('loading');
+    self.getMoar({after:nextPage});
 });
 
+var getMoar= function(qs){
+    qs = (qs?qs:'');
+    $.ajax({
+      url: 'http://reddit.com/.json',
+      dataType: 'json',
+      data: qs,
+      success: function(data){
+        var children = data.data.children;
+        var j = children.length;
+        nextPage = data.data.after;
+        for(var i=0;i<j;++i){
+            $('#theTable').append('<tr><td>'+children[i].data.score+'</td><td>'+children[i].data.title+'</td></tr>');
+        }
+        $('#moarButton').button('reset');
+      }
+    });
+}
