80bb894511c45c0667bea70e2d1a6bdea3f586ef
Tim Kim <timk@adobe.com>
2012-02-08 16:27:11 -0800

    Links now open in child browser

--- ios/O Hai Reddit/www/js/ohaireddit.js
+++ ios/O Hai Reddit/www/js/ohaireddit.js
@@ -18,8 +18,13 @@ var getMoar= function(qs){
         nextPage = data.data.after;
         for(var i=0;i<j;++i){
             // should really template this godawful string out
-            $('#theTable').append('<tr><td><a id="'+children[i].data.id+'" href="#">'+children[i].data.title+'</a></td><td rowspawn="2"><a href="" class="btn"><i class="icon-comment"></i>'+children[i].data.num_comments+'</a></td></tr><tr><td><span class="label label-warning">'+children[i].data.ups+'</span><span class="label label-info">'+children[i].data.subreddit+'</span><span class="label label-success">'+children[i].data.author+'</span></td></tr>');
+            $('#theTable').append('<tr><td id="'+children[i].data.id+'" data-url="'+children[i].data.url+'" class="redditLink"><a href="#">'+children[i].data.title+'</a></td><td rowspawn="2"><a href="" class="btn" style="font-size:50%"><i class="icon-comment"></i>'+children[i].data.num_comments+'</a></td></tr><tr><td><span class="label label-warning">'+children[i].data.ups+'</span><span class="label label-info">'+children[i].data.subreddit+'</span><span class="label label-success">'+children[i].data.author+'</span></td></tr>');
         }
+       
+        $('.redditLink').click(function(){
+            
+            window.plugins.childBrowser.showWebPage($(this).attr('data-url'));
+        });
         $('#moarButton').button('reset');
       }
     });
