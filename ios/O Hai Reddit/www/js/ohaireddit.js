69c25f7a03cbdf2827185d958b0c95ba09931b87
Tim Kim <timk@adobe.com>
2012-02-21 16:33:52 -0800

    Added in templating

--- ios/O Hai Reddit/www/js/ohaireddit.js
+++ ios/O Hai Reddit/www/js/ohaireddit.js
@@ -1,6 +1,22 @@
 var self = this;
 var nextPage = '';
 
+var linkTMPL=['<tr class="redditLinkRow">',
+    '{{#thumbnail}}<a></a>{{/thumbnail}}',
+    '<td id="{{id}}" data-url="{{url}}" class="redditLink"><a>{{title}}</a>',
+        '<div>',
+            '<span class="label label-warning">{{ups}}</span>',
+            '<span class="label label-info">{{subreddit}}</span>',
+            '<span class="label label-success">{{author}}</span>',
+        '</div>',
+    '</td>',
+    '<td class="redditComment" data-permalink="{{permalink}}">',
+        '<a class="btn" style="font-size:50%"><i class="icon-comment"></i>{{numComments}}</a>',
+    '</td>',
+'</tr>'].join('');
+
+
+
 $('#moarButton').on('click',function(){
     $('#moarButton').button('loading');
     $('#commentContainer').hide();
@@ -20,9 +36,23 @@ var getMoar= function(qs){
         nextPage = data.data.after;
         for(var i=0;i<j;++i){
             // should really template this godawful string out
-            $('#theTable').append('<tr class="redditLinkRow"><td id="'+children[i].data.id+'" data-url="'+children[i].data.url+'" class="redditLink"><a>'+children[i].data.title+'</a></td><td class="redditComment" data-permalink="'+children[i].data.permalink+'" rowspawn="2"><a class="btn" style="font-size:50%"><i class="icon-comment"></i>'+children[i].data.num_comments+'</a></td></tr><tr class="redditMetaInfoRow"><td><span class="label label-warning">'+children[i].data.ups+'</span><span class="label label-info">'+children[i].data.subreddit+'</span><span class="label label-success">'+children[i].data.author+'</span></td></tr>');
+            var linkView = {
+                id: children[i].data.id,
+                thumbnail: null,
+                url: children[i].data.url,
+                title: children[i].data.title,
+                ups: children[i].data.ups,
+                subreddit: children[i].data.subreddit,
+                author: children[i].data.author,
+                permalink: children[i].data.permalink,
+                numComments: children[i].data.num_comments
+                
+            };
+            $('#theTable').append(Mustache.render(linkTMPL,linkView));
+
         }
-       
+
+
         $('.redditLink').click(function(){
             window.plugins.childBrowser.showWebPage($(this).attr('data-url'));
         });
